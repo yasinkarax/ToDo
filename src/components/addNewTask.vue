f1f675446c39e4a759997f0f56bc8ecf3a738860
Yasin <yasinkara99@hotmail.com>
2024-07-27 21:47:28 +0300

    feat: sending new tasks to task list

---
+++ src/components/addNewTask.vue
@@ -0,0 +1,37 @@
+<script>
+
+export default {
+    data() {
+        return {
+            head: 'All Tasks',
+            addNewTaskText: 'Add a new task',
+            addNewTaskButton: 'Add Task',
+            NewTask: ''
+        }
+    },
+    methods: {
+        sendNewTask(){
+            this.$emit('newTask', this.NewTask);
+            this.NewTask = '';
+        }
+    }
+}
+</script>
+
+
+<template>
+    <h1>{{ head }}</h1>
+
+    <input 
+        type="text" 
+        :placeholder="addNewTaskText" 
+        v-model="NewTask"
+    >
+    
+    <button @click="sendNewTask">{{ addNewTaskButton }}</button>
+    
+</template>
+
+
+<style lang="sass" scoped>
+</style>

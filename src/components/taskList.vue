5ef6e9e4458887f0dd427a649d0e495e1a720d14
Yasin <yasinkara99@hotmail.com>
2024-07-27 21:50:28 +0300

    feat: listing new tasks which coming from addNewTask.vue

---
+++ src/components/taskList.vue
@@ -0,0 +1,30 @@
+<script>
+    import addNewTask from './addNewTask.vue'
+    export default {
+        data(){
+            return {
+                taskList: []
+            }
+        },
+        methods: {
+            showNewTask(newTask){
+                this.taskList.push(newTask);
+            }
+        },
+        components: {
+            addNewTask
+        }
+    }
+</script>
+<template>
+    <div>
+        <addNewTask @newTask="showNewTask"/>
+        <ul>
+            <li v-for="task in taskList">
+                {{ task }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped></style>
